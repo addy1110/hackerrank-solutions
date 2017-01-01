f281b12719979afb98520a2d5c82432f9ad530ea
addy1110 <addy.1110@gmail.com>
2017-01-01 01:39:51 -0500

    updated full counting sort

--- Hackerrank-Solutions/src/Full_Counting_Sort.java
+++ Hackerrank-Solutions/src/Full_Counting_Sort.java
@@ -1,7 +1,7 @@
-
 import java.util.*;
 import java.text.NumberFormat;
 import java.text.DecimalFormat;
+import java.io.*;
 
 /**
  * Created by ADDY on 31/12/16.
@@ -9,51 +9,62 @@ import java.text.DecimalFormat;
  */
 public class Full_Counting_Sort{
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        scan.nextLine();
+
+//        Scanner scan = new Scanner(System.in);
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        int n=0;
+        try{
+            n = Integer.parseInt(scan.readLine());
+        }catch (IOException io){
+            System.out.println("error reading the line ");
+        }
 //        System.out.println("enter the list items:- ");
 
         List<String> myList = new ArrayList<>(n);
-
+        StringBuilder listElement = new StringBuilder();
         for(int i=0;i<n;i++){
-            String listElement = scan.nextLine();
-            myList.add(listElement);
-        }
+            try{
+                listElement = new StringBuilder(scan.readLine());
 
+            }catch(IOException io){
 
-
-
-        for(int i=0;i<n/2;i++){
-            String temp = myList.get(i);
-            String[] splitTemp = temp.split(" ");
-            temp = splitTemp[0]+" "+"-";
-            myList.set(i, temp);
-        }
-
-//        List<String> myOriginalList = new ArrayList<>(myList);
-
-        Collections.sort(myList, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                String[] a1 = s1.split(" ");
-                String[] a2 = s2.split(" ");
-                return Integer.parseInt(a1[0]) - (Integer.parseInt(a2[0]));
+                System.out.println("error reading the line ");
             }
-        });
+            if(i<n/2){
+                String[] splitTemp = listElement.toString().split(" ");
+                StringBuilder temp = new StringBuilder(splitTemp[0]+" "+"-");
+                myList.add(temp.toString());
+
+            }else{
+                myList.add(listElement.toString());
+            }
+
+        }
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        long startTime = System.currentTimeMillis();
+
+        Collections.sort(myList, (String s1, String s2) -> Integer.parseInt(s1.split(" ")[0]) - Integer.parseInt(s2.split(" ")[0]));
+        long firstLoop = System.currentTimeMillis();
 
 //        System.out.println(myOriginalList);
 //        System.out.println(myList);
+        StringBuilder finalAns = new StringBuilder();
         for(String item: myList){
             String[] ans = item.split(" ");
-            System.out.print(ans[1]+" ");
+            finalAns.append(ans[1]+" ");
         }
+        System.out.println(finalAns);
+        /*for(int i=0;i<n;i++){
+            String[] ans = (new StringBuilder(myList.get(i))).toString().split(" ");
+            System.out.print(ans[1]+" ");
+        }*/
         long endTime   = System.currentTimeMillis();
         long totalTime = endTime - startTime;
         System.out.println();
-        NumberFormat formatter = new DecimalFormat("#0.00000");
-        System.out.print("Execution time is " + formatter.format((totalTime) / 1000d) + " seconds");
+
+        System.out.println("Execution time is " + formatter.format((totalTime) / 1000d) + " seconds");
+        System.out.println("First Loop time " + formatter.format((firstLoop-startTime) / 1000d) + " seconds");
+        System.out.println("Display Loop time " + formatter.format((endTime-firstLoop) / 1000d) + " seconds");
 
 
     }

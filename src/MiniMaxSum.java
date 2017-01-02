8cdb2a509e0f21f40878102f15b18d02ea7b92b0
addy1110 <addy.1110@gmail.com>
2017-01-01 20:46:15 -0500

    added MinMaxSum

--- src/MiniMaxSum.java
+++ src/MiniMaxSum.java
@@ -1,5 +1,23 @@
 /**
  * Created by ADDY on 01/01/17.
  */
+
+import java.util.*;
+
+
 public class MiniMaxSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long[] myList = new long[5];
+        for(int i=0;i<5;i++){
+            myList[i] = in.nextLong();
+        }
+        Arrays.sort(myList);
+
+        Long min = myList[0]+myList[1]+myList[2]+myList[3];
+        Long max = min+myList[4]-myList[0];
+        System.out.print(min);
+        System.out.print(" ");
+        System.out.print(max);
+    }
 }

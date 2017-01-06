52a68c55f3a5aaa4ce3f6675a3df784398d2b5de
addy1110 <addy.1110@gmail.com>
2017-01-06 17:04:40 -0500

    added CaesarCipher

---
+++ src/CaesarCipher.java
@@ -0,0 +1,28 @@
+/**
+ * Created by ADDY on 06/01/17.
+ */
+import java.util.Scanner;
+public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        StringBuilder arr = new StringBuilder(in.nextLine());
+        int k = in.nextInt()%26;
+        for(int i=0;i<n;i++){
+            int index = (int)arr.charAt(i)+k;
+            if((int)arr.charAt(i)>=65 && (int)arr.charAt(i)<=90){
+
+                if (index>90) index = index-26;
+                arr.setCharAt(i,(char)(index)); //65-90
+            }
+            else if((int)arr.charAt(i)>=97 && (int)arr.charAt(i)<=122){
+                if (index>122) index = index-26;
+                arr.setCharAt(i,(char)(index));
+            }
+
+        }
+        System.out.println(arr.toString());
+
+    }
+}

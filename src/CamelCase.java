fa14cf0d37dd8e088fa82042de993a7a0942b654
addy1110 <addy.1110@gmail.com>
2017-01-06 17:04:57 -0500

    added CaesarCipher

---
+++ src/CamelCase.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ADDY on 06/01/17.
+ */
+import java.util.Scanner;
+public class CamelCase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StringBuilder s = new StringBuilder(in.next());
+        int m=0;
+        for(int i=0;i<s.length();i++){
+            if(Character.isUpperCase(s.charAt(i))) m++;
+        }
+        System.out.println(m+1);
+    }
+}

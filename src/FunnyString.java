1efba19312d63fbbee1d33fd2e5759d1d3ab471c
addy1110 <addy.1110@gmail.com>
2017-01-06 18:13:51 -0500

    added FunnyString

---
+++ src/FunnyString.java
@@ -0,0 +1,27 @@
+/**
+ * Created by ADDY on 06/01/17.
+ */
+import java.util.Scanner;
+public class FunnyString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+
+        for(int i=0;i<n;i++){
+            StringBuilder S = new StringBuilder(in.nextLine());
+            StringBuilder SO = new StringBuilder(S);
+            StringBuilder R = new StringBuilder(SO.reverse());
+            boolean isFunny = true;
+            for(int j=0;j<S.length()-1;j++){
+                if(Math.abs(S.charAt(j+1) - S.charAt(j)) != Math.abs(R.charAt(j+1) - R.charAt(j))){
+                    isFunny=false;
+                    break;
+                }
+                else isFunny=true;
+            }
+            if(isFunny) System.out.println("Funny");
+            else System.out.println("Not Funny");
+        }
+    }
+}
